ab09663762e2a011a5e960aa6e7e14b19a5897cf
Michele Digiacomo <m.digiacomo13@studenti.uniba.it>
2021-08-17 10:11:55 +0200

    Test di modifica n°3

--- app/src/main/java/com/example/android/arkanoid/MainActivity.java
+++ app/src/main/java/com/example/android/arkanoid/MainActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //Ciro
+    //Ciro ciccici
 
 
     private Game game;
