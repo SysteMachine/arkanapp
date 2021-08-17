2f1f7a46743a4d123f2be75e592677263df7a554
SysteMachine <64074313+SysteMachine@users.noreply.github.com>
2021-08-17 10:13:48 +0200

    pppp

--- app/src/main/java/com/example/android/arkanoid/MainActivity.java
+++ app/src/main/java/com/example/android/arkanoid/MainActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //Ciro ciccici
+    //Ciro ppppppppp
 
 
     private Game game;
