d6182702aed1833900c730fdb0981de60dc5b9c4
Michele Digiacomo <m.digiacomo13@studenti.uniba.it>
2021-08-24 12:27:03 +0200

    PredisposizioneStili

---
+++ app/src/main/java/com/example/android/arkanoid/GameElements/Stili/ClasseTestStile.java
@@ -0,0 +1,9 @@
+package com.example.android.arkanoid.GameElements.Stili;
+
+import com.example.android.arkanoid.GameElements.BaseElements.Stile;
+
+public class ClasseTestStile extends Stile {
+    public ClasseTestStile(){
+
+    }
+}
